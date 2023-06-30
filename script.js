7d18d82319b2448134fb85958204379c90b87ccc
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2023-07-01 03:22:24 +0530

    authentication_enabled

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js"
+import { initializeApp} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js"
 import { getDatabase,ref,push,set } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js"
-const appSettings = {
-    databaseURL: "https://mun-2023-default-rtdb.firebaseio.com/"
-}
-const app=initializeApp(appSettings)
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAYLIn8hGjgVrX3h23aVZPx47Sn8bZBCz4",
+  authDomain: "mun-2023.firebaseapp.com",
+  databaseURL: "https://mun-2023-default-rtdb.firebaseio.com",
+  projectId: "mun-2023",
+  storageBucket: "mun-2023.appspot.com",
+  messagingSenderId: "843433332162",
+  appId: "1:843433332162:web:faa917397b259754461a5b",
+  measurementId: "G-L3C80FWLKS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+
+
+const analytics = getAnalytics(app);
+
 const database=getDatabase(app)
 
 
@@ -24,22 +49,25 @@ function getInput(id) {
 function submitForm1(e) {
   
     e.preventDefault();
+    
+    signup()
     saveRec1(getInput('name_field'), getInput('email_field'),getInput('password'), getInput('pref1'), getInput('pref2'), getInput('pref3'), getInput('delegatecountry'), getInput('age'),getInput('gender'),getInput('Institute'),getInput('region'),getInput('muncount'),getInput('pastaward'),getInput('refferalcode'),);
    
 }
 function submitForm2(e) {
   
   e.preventDefault();
+  signup()
   saveRec2(getInput('name_field'), getInput('email_field'),getInput('password'), getInput('pref1'), getInput('pref2'), getInput('pref3'), getInput('delegatecountry'), getInput('age'),getInput('gender'),getInput('Institute'),getInput('region'),getInput('muncount'),getInput('pastaward'),getInput('refferalcode'),);
  
 }
-function saveRec1(name, email,password,pref1,pref2,pref3,delegatecountry,age,gender,Institute,region,muncount,pastaward,refferalcode) {
+function saveRec1(name,email,password,pref1,pref2,pref3,delegatecountry,age,gender,Institute,region,muncount,pastaward,refferalcode) {
     const dbRef1 = ref(database, "records of single delegates");
     if(pref1!=pref2&&pref2!=pref3){
     const newRec = push(dbRef1);
     set(newRec, {
-      name: name,
-      email: email,
+      name:name,
+      email:email,
       Password:password,
       delegatecountry: delegatecountry,
       preference1:pref1,
@@ -61,6 +89,7 @@ function saveRec1(name, email,password,pref1,pref2,pref3,delegatecountry,age,gen
     }).catch((error) => {
       // Error handling
       alert("Registration failed: " + error.message);
+
     });
     document.getElementById("registrationForm").reset();
 }
@@ -68,15 +97,15 @@ else{
     alert("Preferences can't be same!")
 }
 }
-function saveRec2(name, email,password,pref1,pref2,pref3,delegatecountry,age,gender,Institute,region,muncount,pastaward,refferalcode) {
+function saveRec2(name,email,password,pref1,pref2,pref3,delegatecountry,age,gender,Institute,region,muncount,pastaward,refferalcode) {
   const dbRef2 = ref(database, "records of Conference ambassadors");
   if(pref1!=pref2&&pref2!=pref3){
   const newRec = push(dbRef2);
   set(newRec, {
-    name: name,
-    email: email,
+    name:name,
+    email:email,
     Password:password,
-    delegatecountry: delegatecountry,
+    delegatecountry:delegatecountry,
     preference1:pref1,
     preference2:pref2,
     preference3:pref3,
@@ -107,44 +136,33 @@ else{
 
 
 
-function sendEmail() {
-  
-  const client = new SMTPClient({
-    user: 'shivanshu264@gmail.com',
-    password: 'AAABF29A67675B4499210685C5077A2BDB87',
-    host: 'smtp.elasticemail.com',
-    ssl: true,
-    port:2525
-  });
-  
-  
-  client.send(
-    {
-      text: 'i hope this works',
-      from: 'shivanshu264@gmail.com>',
-      to: 'shivanshur203@gmail.com>',
-      cc: 'else <shivanshur203@gmail.com>',
-      subject: 'testing emailjs',
-    },
-    (err, message) => {
-      console.log(err || message);
-    }
-  );
-}
 
-document.getElementById("register1").addEventListener("click",function(){
-  const name = document.getElementById("name_field").value;
-  const age = document.getElementById("age").value;
-  const gender = document.getElementById("gender").value;
- 
-  const email = document.getElementById("email_field").value;
-  const password=document.getElementById("password").value;
-  if (name && age && gender   && email && password) {
-   
-    document.getElementById("register2").style.display = "inline";
-    document.getElementById("register3").style.display = "inline";
-    document.getElementById("transformtotext").innerHTML = `<h1 class="newbuttoncont">Register as:</h1>`;
-  } else {
-    
-    alert("Please fill in all the important inputs first.");
-  }})
+  
+  
+
+
+
+
+
+
+
+
+var ab=false;
+  function signup(){
+    var email = document.getElementById("email_field").value;
+
+    console.log(email)
+    var password=document.getElementById("password").value;
+  createUserWithEmailAndPassword(auth,email,password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      ab=true;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      
+      alert(errorMessage);
+      // ..
+    });}
